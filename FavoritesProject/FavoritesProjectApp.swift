9caa7b87012159beb7e03564f6f3cc023ac39a44
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-09-04 15:56:53 +0200

    Initial Commit

---
+++ FavoritesProject/FavoritesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesProjectApp.swift
+//  FavoritesProject
+//
+//  Created by Eric on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoritesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
